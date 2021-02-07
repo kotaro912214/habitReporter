4303ff067d946c6f7332566e6a3b86c73e11d0c2
関倖太郎 <kotaro.seki1216@gmail.com>
2021-02-07 18:48:11 +0900

    ハッシュタグが記載してあるテキストファイルを一つ読み込み、ハッシュタグの数値を抜き出す処理を追加

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
+from enum import Enum
+
+
+class Tag(Enum):
+    weight = 'kg'
+    calorie = 'kcal'
 
 
 def main():
-    print("hello")
+    datalist = read_data('sample.txt')
+    weight_list = []
+    calorie_list = []
+    for data in datalist:
+        if Tag.weight.value in data:
+            weight_list.append(extract_value(data))
+            continue
+        if Tag.calorie.value in data:
+            calorie_list.append(extract_value(data))
+            continue
+    print(weight_list)
+    print(calorie_list)
+
+
+def read_data(filepath):
+    f = open(filepath, 'r', encoding='UTF-8')
+    datalist = f.readlines()
+    f.close()
+    return datalist
+
+
+def extract_value(data):
+    a, b = data.split()
+    if '#' in a:
+        return b
+    return a
+
 
 if __name__ == '__main__':
     main()
