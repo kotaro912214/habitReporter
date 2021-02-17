6492c391a69c7774f4ebcc60aedd1d75965b340a
関倖太郎 <kotaro.seki1216@gmail.com>
2021-02-18 01:19:02 +0900

    何度もアクセスする可能性があるプロパティに関してはインスタンス変数として保持するよう修正。新たに任意のlabelがis_binary指定なのかを返すメソッドを追加。

--- config.py
+++ config.py
@@ -4,18 +4,27 @@ import json
 class Config:
     def __init__(self):
         self.config = None
+        self.labels = None
+        self.items = None
 
     def read_config(self):
         self.config = json.load(open("config.json", "r"))
+        self.items = self.config["items"]
+        self.labels = [item["label"] for item in self.items]
 
     def get_base_dir(self) -> str:
         return self.config["base_dir"]
 
     def get_items(self) -> [object]:
-        return self.config["items"]
+        return self.items
 
     def get_labels(self) -> [str]:
-        return [item["label"] for item in self.config["items"]]
+        return self.labels
+
+    def is_binary(self, label) -> bool:
+        for item in self.items:
+            if item["label"] == label:
+                return item["is_binary"]
 
 
 if __name__ == "__main__":
